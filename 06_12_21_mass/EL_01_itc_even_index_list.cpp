e98cecd2326d684ff75f668cba85c9b65c94702e
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 12:39:40 +0300

    Update EL_01_itc_even_index_list.cpp

--- 06_12_21_mass/EL_01_itc_even_index_list.cpp
+++ 06_12_21_mass/EL_01_itc_even_index_list.cpp
@@ -2,5 +2,6 @@
 
 void itc_even_index_list(const vector <int>& mass, vector <int>& mass2)
 {
+	if (mass == vector<int>{ }) return;
 	for (int i = 0; i < mass.size(); i++) if (i % 2 == 0) mass2.push_back(mass[i]);
 }
