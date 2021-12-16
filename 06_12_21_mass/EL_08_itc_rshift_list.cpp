254cb060cb183b6f5179ca47f006db5fab4a227d
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 12:34:37 +0300

    Update EL_08_itc_rshift_list.cpp

--- 06_12_21_mass/EL_08_itc_rshift_list.cpp
+++ 06_12_21_mass/EL_08_itc_rshift_list.cpp
@@ -2,6 +2,7 @@
 
 void itc_rshift_list(vector <int>& mass)
 {
+	if (mass == vector<int>{ }) return;
 	int temp, temp2 = mass[mass.size() - 1];
 	for (int i = 0; i < mass.size(); i++) {
 		temp = mass[i];
