dd8648c563664a1214c4d052a904619ade18a8cf
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 12:38:58 +0300

    Update EL_03_itc_positive_list.cpp

--- 06_12_21_mass/EL_03_itc_positive_list.cpp
+++ 06_12_21_mass/EL_03_itc_positive_list.cpp
@@ -2,6 +2,7 @@
 
 int itc_positive_list(const vector <int>& mass)
 {
+	if (mass == vector<int>{ }) return 0;
 	int count = 0;
 	for (int i = 0; i < mass.size(); i++) if (mass[i] > 0) count++;
 	return count;
