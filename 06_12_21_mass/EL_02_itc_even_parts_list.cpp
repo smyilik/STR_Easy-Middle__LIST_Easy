81c451473bb64d239ad7adcdd0801d965a137911
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 12:39:21 +0300

    Update EL_02_itc_even_parts_list.cpp

--- 06_12_21_mass/EL_02_itc_even_parts_list.cpp
+++ 06_12_21_mass/EL_02_itc_even_parts_list.cpp
@@ -2,5 +2,6 @@
 
 void itc_even_parts_list(const vector <int>& mass, vector <int>& mass2)
 {
+	if (mass == vector<int>{ }) return;
 	for (int i = 0; i < mass.size(); i++) if (mass[i] % 2 == 0) mass2.push_back(mass[i]);
 }
