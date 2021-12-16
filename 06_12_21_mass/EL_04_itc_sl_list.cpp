96ddd4cd0b1f6deb2f8b26554fd8dda0cba0a436
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 12:38:17 +0300

    Update EL_04_itc_sl_list.cpp

--- 06_12_21_mass/EL_04_itc_sl_list.cpp
+++ 06_12_21_mass/EL_04_itc_sl_list.cpp
@@ -2,6 +2,7 @@
 
 int itc_sl_list(const vector <int>& mass)
 {
+	if (mass == vector<int>{ }) return 0;
 	int count = 0;
 	for (int i = 1; i < mass.size(); i++) if (mass[i] > mass[i - 1]) count++;
 	return count;
